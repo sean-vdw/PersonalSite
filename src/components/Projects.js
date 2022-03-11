41f1cc9cc33a4a26ff6624474074c8d109d0529e
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-03-11 13:17:25 -0800

    updated hero and projects; mapping over projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,34 +1,59 @@
-import { useEffect } from 'react';
-
-import gsap from "gsap";
 import { ExternalLinkIcon } from '@heroicons/react/solid';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
+// project image imports
 import estatesync from '../assets/estatesync.png';
+import ifaas from '../assets/ifaas.png';
 import dotts from '../assets/dotts.png';
 
+// Project icon variables
+const javascriptIcon = 'fab fa-js-square';
+const htmlIcon = 'fab fa-html5';
+const cssIcon = 'fab fa-css3-alt';
+const reactIcon = 'fab fa-react';
+const nodeIcon = 'fab fa-node-js';
+
+// Projects array to map over
+const projects = [
+  {
+    image: estatesync,
+    status: 'Deployed',
+    name: 'EstateSync',
+    description: 'Synchronized estate management. EstateSync removes the burden from the fragmented experience of estate management by aligning assets, people, and process on a single platform.',
+    url: 'https://estatesync.co',
+    languageIcons: [
+      reactIcon,
+      javascriptIcon,
+      htmlIcon,
+      cssIcon
+    ]
+  },
+  {
+    image: ifaas,
+    status: 'Deployed',
+    name: 'IFaaS',
+    description: 'IFaaS (Investing Framework as a Service) provides an institutional-quality investing framework for high-growth companies. This utilizes APIs and charts from 3rd party libraries',
+    url: 'https://ifaas.vercel.app',
+    languageIcons: [
+      reactIcon,
+      javascriptIcon,
+      cssIcon
+    ]
+  },
+  {
+    image: dotts,
+    status: 'In Progress',
+    name: 'Dotts.io',
+    description: 'The best messaging experience ever made. Dotts.io is a new messaging app that puts user experience first with the widest range of customizable features available.',
+    url: 'https://dotts.vercel.app',
+    languageIcons: [
+      javascriptIcon,
+      reactIcon,
+      nodeIcon
+    ]
+  }
+];
+
 export default function Projects() {
-  gsap.registerPlugin(ScrollTrigger);
-
-  useEffect(() => {
-    gsap.from('#esImg', {
-      scrollTrigger: {
-        trigger: '#esImg'
-      },
-      y: 50,
-      duration: 0.5,
-      opacity: 0
-    });
-
-    gsap.from('#esAbout', {
-      scrollTrigger: {
-        trigger: '#esAbout'
-      },
-      y: 50,
-      duration: 0.5,
-      opacity: 0
-    });
-  }, []);
 
   return (
     <div className='bg-zinc-900'>
@@ -36,65 +61,36 @@ export default function Projects() {
         <h2 className="text-3xl w-1/3 pb-4 text-white text-left font-extrabold tracking-tight sm:text-4xl border-b-8 border-teal-700">
           Projects
         </h2>
-
-        {/* PROJECT ONE */}
-        <div className="relative w-full mt-12 lg:mt-12 flex-col justify-center align-middle">
-          <img
-            id='esImg' 
-            className="rounded-xl w-full md:w-10/12 object-cover"
-            src={estatesync}
-            alt="Project image"
-          />
-          <div id='esAbout' className="w-full md:w-2/3 lg:w-1/2 md:mr-20 relative md:absolute md:-right-2 top-10 md:top-20 lg:top-14 text-left py-6 px-8 shadow-2xl bg-zinc-800 border-t-4 border-teal-700">
-            <p className="text-gray-300 text-sm uppercase">Deployed</p>
-            <h2 className="text-white pb-1">EstateSync</h2>
-            <p className="text-white text-sm leading-7"><b>Synchronized estate management. </b>EstateSync removes the burden from the fragmented experience of estate management by aligning assets, people, and process on a single platform. </p>
-            <a href='https://estatesync.co/' target='_blank' className='no-underline'>
-              <button
-                type="button"
-                className="inline-flex items-center px-4 py-2 shadow-sm text-base font-medium rounded-md text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
-              >
-                <ExternalLinkIcon className="-ml-1 mr-3 h-5 w-5" aria-hidden="true" />
-                Check it out
-              </button>
-            </a>
-            <div className='flex justify-between w-7/12 md:w-1/3 mt-4'>
-              <i className="fab fa-js-square fa-2x text-zinc-500 ease-in-out duration-200 hover:-translate-y-2"></i>
-              <i className="fab fa-html5 fa-2x text-zinc-500 ease-in-out duration-200 hover:-translate-y-2"></i>
-              <i className="fab fa-css3-alt fa-2x text-zinc-500 ease-in-out duration-200 hover:-translate-y-2"></i>
-              <i className="fab fa-react fa-2x text-zinc-500 ease-in-out duration-200 hover:-translate-y-2"></i>
+        {projects.map(project => {
+          return (
+            <div className="relative w-full mt-12 lg:mt-12 flex-col justify-center align-middle">
+              <img
+                className="rounded-xl w-full md:w-10/12 object-cover"
+                src={project.image}
+                alt={`${project.name} Teaser`}
+              />
+              <div id='esAbout' className="w-full md:w-2/3 lg:w-1/2 md:mr-20 relative md:absolute md:-right-2 top-10 md:top-20 lg:top-14 text-left py-6 px-8 shadow-2xl bg-zinc-800 border-t-4 border-teal-700">
+                <p className="text-gray-300 text-sm uppercase">{project.status}</p>
+                <h2 className="text-white pb-1">{project.name}</h2>
+                <p className="text-white text-sm leading-7">{project.description}</p>
+                <a href={project.url} target='_blank' className='no-underline'>
+                  <button
+                    type="button"
+                    className="inline-flex items-center px-4 py-2 shadow-sm text-base font-medium rounded-md text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+                  >
+                    <ExternalLinkIcon className="-ml-1 mr-3 h-5 w-5" aria-hidden="true" />
+                    Check it out
+                  </button>
+                </a>
+                <div className='flex justify-between w-7/12 md:w-1/3 mt-4'>
+                  {project.languageIcons.map(icon => {
+                    return <i className={`${icon} fa-2x text-zinc-500 ease-in-out duration-200 hover:-translate-y-2`}></i>
+                  })}
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-
-        {/* PROJECT TWO */}
-        <div className="relative w-full mt-28 flex-col justify-center align-middle">
-          <img
-            id='esImg' 
-            className="rounded-xl w-full md:w-10/12 object-cover"
-            src={dotts}
-            alt="Project image"
-          />
-          <div id='esAbout' className="w-full md:w-2/3 lg:w-1/2 md:mr-20 relative md:absolute md:-right-2 top-10 md:top-20 lg:top-14 text-left py-6 px-8 shadow-2xl bg-zinc-800 border-t-4 border-teal-700">
-            <p className="text-gray-300 text-sm uppercase">In Progress</p>
-            <h2 className="text-white pb-1">Dotts.io</h2>
-            <p className="text-white text-sm leading-7"><b>The best messaging experience ever made. </b>Dotts.io is a new messaging app that puts user experience first with the widest range of customizable features available.</p>
-            <a href='https://dotts.vercel.app/' target='_blank' className='no-underline'>
-              <button
-                type="button"
-                className="inline-flex items-center px-4 py-2 shadow-sm text-base font-medium rounded-md text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
-              >
-                <ExternalLinkIcon className="-ml-1 mr-3 h-5 w-5" aria-hidden="true" />
-                Check it out
-              </button>
-            </a>
-            <div className='flex justify-between w-6/12 md:w-1/3 mt-4'>
-              <i className="fab fa-js-square fa-2x text-zinc-500 ease-in-out duration-200 hover:-translate-y-2"></i>
-              <i className="fab fa-node-js fa-2x text-zinc-500 ease-in-out duration-200 hover:-translate-y-2"></i>
-              <i className="fab fa-react fa-2x text-zinc-500 ease-in-out duration-200 hover:-translate-y-2"></i>
-            </div>
-          </div>
-        </div>
+          )
+        })}
       </div>
     </div>
   )
